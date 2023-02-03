79eb7f65d836326abc06f9777540163a0f9abb57
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:29:52 -0600

    Add files via upload

---
+++ CreatrNameFile/CreatrNameFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatrNameFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream("Names.txt",
+                FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(file);
+
+            string[] names = {"Anthony ",
+                              "Belle   ",
+                              "Carolyn ",
+                              "David   ",
+                              "Edwin   ",
+                              "Frannie ",
+                              "Gina    ",
+                              "Hannah  ",
+                              "Inez    ",
+                              "Juan    "};
+            int x;
+            for (x = 0; x < names.Length; ++x)
+            { 
+                writer.WriteLine(names[x]);
+            }
+            writer.Close();
+            file.Close();
+            
+        }
+    }
+}
