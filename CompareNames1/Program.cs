be900333048f6e97e5607c016fbf56b338554692
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:41:31 -0600

    Add files via upload

---
+++ CompareNames1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Written by <Tam M. Nguyen>
+//Written on <11/1/19>
+using static System.Console;
+
+namespace CompareNames1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name1 = "Amy";
+            string name2 = "Amy";
+            string name3 = "Mathew";
+
+            WriteLine("compare {0} to {1}: {2}",
+                name1, name2, name1 == name2);
+            WriteLine("compare {0} to {1}: {2}",
+                name1, name3, name1 == name3);
+        }
+    }
+}
