2e3a49a5b1ee2bbda4a65ce28d08fc277f8462ee
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:16:00 -0600

    Add files via upload

---
+++ FiveColor2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiveColor2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(204, 0, 0);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(255, 128, 0);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(102, 204, 0);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(255, 102, 0);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(0, 102, 204);
+        }
+    }
+}
