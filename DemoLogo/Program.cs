a3d4c668845b7f8a03957f977356e77ce8a91c47
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:05:18 -0600

    Add files via upload

---
+++ DemoLogo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+
+namespace DemoLogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Our company is ");
+            DisplayCompanyLogo();
+        }
+        private static void DisplayCompanyLogo()
+        {
+            WriteLine("See Sharp Optical");
+            WriteLine("We prize your eyes");
+        }
+    }
+}
