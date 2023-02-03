d2658f6e6565b99cb50f9f070ff5fdfddbd90085
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:04:44 -0600

    Add files via upload

---
+++ ConvertQuartsToLiters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+
+namespace ConvertQuartsToLiters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            double quarts = double.Parse(ReadLine());
+            double liters = ConversionCalcQuartsIntoLiters(quarts);
+            WriteLine("Enter a number of quarts and we'll calculate how many quarts equal to liters.");
+          
+            WriteLine("{0} quarts is total to {1} liters", quarts, liters);
+           
+            ReadLine();
+        }
+        private static double ConversionCalcQuartsIntoLiters(double quarts)
+        {
+            double quartz = .966353;
+            double liters = quarts * quartz;
+            WriteLine("A quart is {0} liters", quartz);
+
+            return liters;
+        }
+        
+
+    }
+}
