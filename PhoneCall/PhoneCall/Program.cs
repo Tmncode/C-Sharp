7dd42895df8b02c9eae464e55525a171ccc91a55
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:35:26 -0600

    Add files via upload

---
+++ PhoneCall/PhoneCall/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+
+namespace PhoneCall
+{
+    class Program
+    {
+        private static double CalcPhoneCallPrice(int minutes, double rates)
+        {
+            const double BASE_FEE = 0.25;
+            double callFee;
+            callFee = BASE_FEE + minutes * rates;
+            return callFee;
+        }
+        static void Main()
+        {
+            int[] callLengths = { 2, 5 };
+            double[] rates = { 0.03, 0.12 };
+            double priceOfCall;
+
+            WriteLine("{0, 10}{1, 10}{2, 10}",
+                "Minutes", "Rate", "Price");
+
+            for(int x = 0; x < callLengths.Length; ++x)
+                for(int y = 0; y < rates.Length; ++y)
+            {
+                priceOfCall = CalcPhoneCallPrice(callLengths[x], rates[y]);
+                WriteLine("{0, 10}{1, 10}{2, 10}",
+                    callLengths[x], rates[y], priceOfCall.ToString("C"));
+            }
+            ReadLine();
+        }
+    }
+}
