8f144755d6f7b841e9f99959b9b4548c24aad4de
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:07:40 -0600

    Add files via upload

---
+++ OverloadedTriples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedTriples
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 20;
+            string message = "Go team!";
+            Triple(num);
+            Triple(message);
+            Read();
+        }
+        private static void Triple(int num)
+        {
+            const int MULT_FACTOR = 3;
+            WriteLine("{0} times {1} is {2}\n",
+                num, MULT_FACTOR, num * MULT_FACTOR);
+        }
+        private static void Triple(string message)
+        {
+            WriteLine("{0}\t{0}\t{0}\n", message);
+        }
+    }
+}
