51fd95dd0d4de1dcec4dac35f7c42ad20ba6ba56
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:01:33 -0600

    Add files via upload

---
+++ SumInts/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+
+namespace SumInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0, total = 0;
+            const int LIMIT = 999;
+
+            string input;
+
+            Write("User enter any number   ");
+
+            input = ReadLine();
+            number = Convert.ToInt32(input);
+
+            while(!(number == LIMIT))
+            {
+                total += number;
+                WriteLine("{0}", total);
+                //for(number = 0; number <= LIMIT; ++number)
+                //{
+                //    WriteLine("{0}", number);
+                //}
+                Write("User enter any number   ");
+
+                input = ReadLine();
+                number = Convert.ToInt32(input);
+            }
+
+                WriteLine("Goodbye!");
+            ReadLine();
+        }
+    }
+}
