4ac424ef2d0c1c32835a4ca5cba167058e7ee465
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:19:13 -0600

    Add files via upload

---
+++ WordsOfWisdom/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordsOfWisdom
+{
+    public partial class QuoteForm : Form
+    {
+        public QuoteForm()
+        {
+            InitializeComponent();
+        }
+
+        private void QuoteForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.White;
+        }
+
+        private void label2_MouseHover(object sender, EventArgs e)
+        {
+            label2.BackColor = Color.White;
+        }
+
+        private void label3_MouseHover(object sender, EventArgs e)
+        {
+            label3.BackColor = Color.White;
+        }
+
+        private void label4_MouseHover(object sender, EventArgs e)
+        {
+            label4.BackColor = Color.White;
+        }
+    }
+}
