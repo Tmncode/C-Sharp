6bfa70f5001cfa2e0fab45e27431b1d574eb1884
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:31:03 -0600

    Add files via upload

---
+++ FindPatientRecords/FindPatientRecords/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPatientRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream file = new FileStream(@"D:\C#\CSharp.Chapter.14\CustomersInfo.txt",
+                FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+
+            Console.Write("Enter ID: ");
+            string id = Console.ReadLine(), patientInfo = reader.ReadLine();
+
+            while (!string.IsNullOrEmpty(patientInfo))
+            {
+                if (Regex.IsMatch(patientInfo, id))
+                {
+                    Console.WriteLine($"Found customer {patientInfo}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Customer does not exist");
+                }
+                patientInfo = reader.ReadLine();
+            }
+            file.Close();
+            reader.Close();
+            Console.ReadLine();
+        }
+    }
+}
