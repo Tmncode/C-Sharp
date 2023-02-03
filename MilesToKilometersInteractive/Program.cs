325ea2c71d1fe773803640e9765c7d303b6aeaae
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:45:02 -0600

    Add files via upload

---
+++ MilesToKilometersInteractive/Program.cs
@@ -0,0 +1,21 @@
+﻿//Written by <Tam M. Nguyen>
+//Written on <11/5/19>
+using System;
+using static System.Console;
+
+namespace MilesToKilometersInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Miles;
+            double Kilometer;
+            Write("enter amount of miles  ");
+            Miles = Convert.ToDouble(ReadLine());
+            Kilometer = 1.6 * Miles;
+            WriteLine("{0} Miles into Kilometers is {1}", Miles, Kilometer);
+
+        }
+    }
+}
