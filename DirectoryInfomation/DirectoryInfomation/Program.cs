e20dbdbd60e408aba1e9a99ef00c6af3b88ffea7
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:30:28 -0600

    Add files via upload

---
+++ DirectoryInfomation/DirectoryInfomation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Services;
+
+namespace DirectoryInfomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string END = "end";
+            string dirName;
+            
+            Console.Write("Enter a folder, type end to quit >> ");
+            dirName = Console.ReadLine();
+            while (dirName.ToLower() != END)
+            {
+                bool exists = Directory.Exists(dirName);
+                Console.WriteLine($"Directory exists? " + exists);
+                if (exists)
+                {
+                    foreach (string s in Directory.GetFiles(dirName))
+                    {
+                        Console.WriteLine($"\t{s}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Directory does not exists");
+                }
+                Console.Write("Enter a folder, type end to quit >> ");
+                dirName = Console.ReadLine();      
+            }
+            
+            Console.ReadLine();
+        }
+        
+    }
+}
