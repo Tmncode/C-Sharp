131d019290a6fd22601007f71a390e231ae07ec4
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:42:01 -0600

    Add files via upload

---
+++ DemoEscapeSequences/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+//Written by <Tam M. Nguyen>
+//Written on <11/1/19>
+namespace DemoEscapeSequences
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine("This line\tcontains two\ttabs");
+            WriteLine("This statement\ncontains a new line");
+            WriteLine("This statement sounds three alerts\a\a\a");
+
+
+        }
+    }
+}
