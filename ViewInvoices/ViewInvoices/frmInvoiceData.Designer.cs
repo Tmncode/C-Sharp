e579e4d34be97b92d650f789819430709e2a444e
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:32:56 -0600

    Add files via upload

---
+++ ViewInvoices/ViewInvoices/frmInvoiceData.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ViewInvoices
+{
+    partial class frmInvoiceData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            reader.Close();
+            file.Close();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.viewButton = new System.Windows.Forms.Button();
+            this.lblInvoice = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.invoiceBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.amountBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstruction.Location = new System.Drawing.Point(61, 45);
+            this.lblInstruction.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(139, 38);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Click the button\r\nto view records.\r\n";
+            this.lblInstruction.Click += new System.EventHandler(this.lblInstruction_Click);
+            // 
+            // viewButton
+            // 
+            this.viewButton.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewButton.Location = new System.Drawing.Point(251, 45);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(100, 40);
+            this.viewButton.TabIndex = 1;
+            this.viewButton.Text = "View record";
+            this.viewButton.UseVisualStyleBackColor = true;
+            this.viewButton.Click += new System.EventHandler(this.ViewButton_Click);
+            // 
+            // lblInvoice
+            // 
+            this.lblInvoice.AutoSize = true;
+            this.lblInvoice.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvoice.Location = new System.Drawing.Point(83, 122);
+            this.lblInvoice.Name = "lblInvoice";
+            this.lblInvoice.Size = new System.Drawing.Size(46, 14);
+            this.lblInvoice.TabIndex = 2;
+            this.lblInvoice.Text = "Invoice";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(83, 188);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 14);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.Location = new System.Drawing.Point(83, 259);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(51, 14);
+            this.lblAmount.TabIndex = 4;
+            this.lblAmount.Text = "Amount";
+            // 
+            // invoiceBox
+            // 
+            this.invoiceBox.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.invoiceBox.Location = new System.Drawing.Point(208, 112);
+            this.invoiceBox.Name = "invoiceBox";
+            this.invoiceBox.Size = new System.Drawing.Size(154, 22);
+            this.invoiceBox.TabIndex = 5;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(208, 178);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(154, 22);
+            this.nameBox.TabIndex = 6;
+            // 
+            // amountBox
+            // 
+            this.amountBox.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountBox.Location = new System.Drawing.Point(208, 249);
+            this.amountBox.Name = "amountBox";
+            this.amountBox.Size = new System.Drawing.Size(154, 22);
+            this.amountBox.TabIndex = 7;
+            // 
+            // frmInvoiceData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(420, 352);
+            this.Controls.Add(this.amountBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.invoiceBox);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblInvoice);
+            this.Controls.Add(this.viewButton);
+            this.Controls.Add(this.lblInstruction);
+            this.Font = new System.Drawing.Font("Tahoma", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "frmInvoiceData";
+            this.Text = "Invoice Data";
+            this.Load += new System.EventHandler(this.frmInvoiceData_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Button viewButton;
+        private System.Windows.Forms.Label lblInvoice;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.TextBox invoiceBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox amountBox;
+    }
+}
+
---
+++ ViewInvoices/ViewInvoices/frmInvoiceData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ViewInvoices
+{
+    public partial class frmInvoiceData : Form
+    {
+        const char DELIM = ',';
+        const string FILENAME = @"D:\C#\CSharp.Chapter.14\Invoices.txt";
+        string recordIn;
+        string[] fields;
+        static FileStream file = new FileStream(FILENAME, 
+            FileMode.Open, FileAccess.Read);
+        StreamReader reader = new StreamReader(file);
+        public frmInvoiceData()
+        {
+            InitializeComponent();
+        }
+
+        private void lblInstruction_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ViewButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                recordIn = reader.ReadLine();
+                fields = recordIn.Split(DELIM);
+                invoiceBox.Text = fields[0];
+                nameBox.Text = fields[1];
+                amountBox.Text = fields[2];
+            }
+            catch (NullReferenceException)
+            {
+                lblInstruction.Text = "You have viewed\nall the records";
+                viewButton.Enabled = false;
+            }
+        }
+
+        private void frmInvoiceData_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
