c2f84ecaa0929bd8a0cd31b57cddff3736c48793
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:03:01 -0600

    Add files via upload

---
+++ ArrayDemo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace ArrayDemo1
+{
+    class ArrayDemo1
+    {
+        static void Main()
+        {
+            double[] payRate = { 6.00, 7.35, 8.12, 12.45 };
+
+            for (int x = 0; x < payRate.Length; ++x)
+            {
+                WriteLine("Pay rate {0} is {1}",
+                    x, payRate[x].ToString("C"));
+            }
+            WriteLine("\nPress the Enter key to close program.");
+            ReadLine();
+        }
+    }
+}
