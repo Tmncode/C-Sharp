2788a65b556f7eb4b09c5614d04adfc8a14be7ab
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:30:14 -0600

    Add files via upload

---
+++ CustomizedAForm/CustomizedAForm/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CustomizedAForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.colorListBox = new System.Windows.Forms.ListBox();
+            this.maskedWidth = new System.Windows.Forms.MaskedTextBox();
+            this.maskedHeight = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(233, 348);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(167, 77);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // colorListBox
+            // 
+            this.colorListBox.FormattingEnabled = true;
+            this.colorListBox.ItemHeight = 31;
+            this.colorListBox.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Orange",
+            "Purple",
+            "Black",
+            "Brown",
+            "Blue",
+            "Yellow"});
+            this.colorListBox.Location = new System.Drawing.Point(33, 81);
+            this.colorListBox.Name = "colorListBox";
+            this.colorListBox.Size = new System.Drawing.Size(203, 221);
+            this.colorListBox.TabIndex = 1;
+            // 
+            // maskedWidth
+            // 
+            this.maskedWidth.Location = new System.Drawing.Point(276, 264);
+            this.maskedWidth.Name = "maskedWidth";
+            this.maskedWidth.Size = new System.Drawing.Size(100, 38);
+            this.maskedWidth.TabIndex = 2;
+            // 
+            // maskedHeight
+            // 
+            this.maskedHeight.Location = new System.Drawing.Point(467, 264);
+            this.maskedHeight.Name = "maskedHeight";
+            this.maskedHeight.Size = new System.Drawing.Size(100, 38);
+            this.maskedHeight.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dimension";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(270, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 36);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Title";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(276, 81);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(291, 38);
+            this.txtTitle.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 36);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Color";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(270, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Width";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(461, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 32);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Height";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 452);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedHeight);
+            this.Controls.Add(this.maskedWidth);
+            this.Controls.Add(this.colorListBox);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Customize Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox colorListBox;
+        private System.Windows.Forms.MaskedTextBox maskedWidth;
+        private System.Windows.Forms.MaskedTextBox maskedHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
