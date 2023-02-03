5f8375f6ca2ef9db6096cca4508d2ab109f5d7c7
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:00:46 -0600

    Add files via upload

---
+++ DisplayMultiplicationTable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+
+namespace DisplayMultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, i;
+            int n = 10;
+            // Indent column headers
+            Write("{0, 4}", null);                     
+            // Write column headers
+            for (j = 1; j <= n; j++)
+                Write("{0, 4}", j);
+
+            // Write column header seperator
+            WriteLine();
+            WriteLine("_____________________________________________________");
+
+            // Write the table
+            for (j=1; j<=n; j++)
+            {
+                // Write the row header
+                Write("{0, 4}", j);
+
+                for (i = 1; i <= n; i++)
+                {
+                    // Write the row values
+                    Write("{0, 4}", j * i);
+                }
+                // Finish the line
+                WriteLine();
+
+            }
+            ReadLine();
+        }
+    }
+}
