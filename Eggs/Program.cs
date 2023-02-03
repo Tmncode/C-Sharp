5d7253b274338b5350a8aa3fbfb0c12e0e308fd9
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:44:16 -0600

    Add files via upload

---
+++ Eggs/Program.cs
@@ -0,0 +1,27 @@
+﻿//Written by <Tam M. Nguyen>
+//Written on <11/6/2019>
+
+using System;
+using static System.Console;
+
+namespace Eggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int chicken1 = 35, chicken2 = 29, chicken3 = 30, chicken4 = 33,
+                sum, dozen, remainder;
+
+            sum = chicken1 + chicken2 + chicken3 + chicken4;
+            dozen = sum / 12;
+            remainder = sum % 12;
+
+            Console.WriteLine("I have 4 chicken laid {0} eggs this month.  That is {1} dozen and {2} remaining.",
+                 chicken1 + chicken2 + chicken3 + chicken4, dozen, remainder); 
+
+            ReadLine();
+        }
+    }
+}
