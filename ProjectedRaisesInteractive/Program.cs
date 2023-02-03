b4cf301e9529a51e2c470d6ff74358abf79d1476
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:45:43 -0600

    Add files via upload

---
+++ ProjectedRaisesInteractive/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace ProjectedRaisesInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salaries;
+            
+            Write("Enter salaries amount  ");
+            salaries = Convert.ToDouble(ReadLine());
+            salaries += 0.50 * salaries;
+            WriteLine("{0} is the raise", salaries);
+
+        }
+    }
+}
