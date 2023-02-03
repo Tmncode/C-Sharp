d1ce5606783b55c521f0f7928b503b0123a2fd75
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:46:11 -0600

    Add files via upload

---
+++ variable declared test num 6/Program.cs
@@ -0,0 +1,32 @@
+﻿//Written by <Tam Nguyen>
+//Written on <11/4/2019>
+using System;
+using static System.Console;
+
+
+namespace MilesToKilometersInteractive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double MilesToKilometers = 1.609;
+            WriteLine("MilesToKilometers");
+            ReadLine();
+            
+
+
+            
+            
+            
+           
+            
+            
+            
+            
+          
+         
+            
+        }
+    }
+}
