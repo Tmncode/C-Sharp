4ee8ed818d839e97cbd3a61395aff3269aa584f1
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:06:38 -0600

    Add files via upload

---
+++ Averages/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Averages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            string input;
+
+            Write("Enter a numbers: " + Environment.NewLine);
+
+            while (true)
+            {
+                input = ReadLine();
+                int number = -1;
+                bool success = int.TryParse(input, out number);
+
+                if (success)
+                    numbers.Add(number);
+                else
+                    break;  
+            }
+
+            
+            WriteLine("Count:" + numbers.Count);
+            
+            WriteLine("Average:" + numbers.Average());
+            ReadLine();
+        }
+
+    }
+}
