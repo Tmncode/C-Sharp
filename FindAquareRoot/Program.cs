15f9f22478257b2014928e181d4fb44ec0f8857b
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:13:55 -0600

    Add files via upload

---
+++ FindAquareRoot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindSquareRoot
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool isValid = false;
+            double number = 0, sqrt = 0;
+            while (!isValid)
+            {
+                Write("Enter a valid number: ");
+                isValid = double.TryParse(ReadLine(), out number);
+            }
+            try
+            {
+                if (number < 0)
+                {
+                    throw new ApplicationException("Can't be a negative number");
+                }
+                else
+                {
+                    sqrt = Math.Sqrt(number);
+                }
+            }
+            catch (ApplicationException e)
+            {
+                WriteLine(e.Message);
+            }
+            WriteLine("Square Root of {0} is {1}", number, sqrt);
+            ReadKey();
+        }
+    }
+}
