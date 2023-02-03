58017677deb6faec8aa9f082ec549dcdf9f999ef
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:05:31 -0600

    Add files via upload

---
+++ Multiplication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a number : ");
+            string input = ReadLine();
+            int num;
+            int.TryParse(input, out num);
+            WriteLine();
+
+            DisplayMultiplicationTable(num);
+            ReadLine();
+        }
+        private static void DisplayMultiplicationTable(int num)
+        {
+            for(int i = 2; i <= 10; i++)
+            {
+                WriteLine("{0} * {1, -2} = {2}", num, i, num * i);
+            }
+        }
+    }
+}
