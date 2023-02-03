09e4b1d070217253a800be960bc610267cabcb59
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 20:42:54 -0600

    Add files via upload

---
+++ DemoVariables/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace DemoVariables
+{
+    class Demovariables
+    {
+        static void Main()
+        {
+            long anInt = -123456789000;
+            int anUnsignedInt = -567;
+        WriteLine("The int is {0} and the unsigned int is {1}.",
+            anInt, anUnsignedInt);
+        }
+    }
+}
