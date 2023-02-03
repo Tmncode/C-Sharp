eef96846e83b40c3885d71e330f1b22f9d446933
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:04:59 -0600

    Add files via upload

---
+++ CountVowelsModulrized/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+
+namespace CountVowelsModulrized
+{
+    class Program
+    {
+       static void Main()
+       {
+            int total = 0;
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            Write("I got my car wash yesterday after school.\n");
+
+            string sentence = ReadLine().ToLower();
+            sentence = "I got my car wash yesterday after school.";
+
+            total = sentence.Count(x => vowels.Contains(x));
+
+            WriteLine("Your total number of vowels in the sentence is: {0}", total);
+
+            ReadLine();
+       }
+    }
+}
