8f9cad13922f4e30c3fe6e62c34b8b1457b4c175
Tmncode <88951119+Tmncode@users.noreply.github.com>
2023-02-02 21:10:13 -0600

    Add files via upload

---
+++ TaxpayerDemo2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxpayerDemo2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Taxpayer2[] arrayTax = new Taxpayer2[2];
+            int x;
+            for (x = 0; x < arrayTax.Length; ++x)
+            {
+                arrayTax[x] = new Taxpayer2();
+
+                WriteLine(arrayTax[x]);
+                WriteLine("Enter your Social Security Number.");
+                arrayTax[x].SSN = ReadLine();
+
+                WriteLine("Enter your yearly income salary.");
+                arrayTax[x].GrossIncome = double.Parse(ReadLine());
+                WriteLine();
+
+                WriteLine(" Your Social Security Number {0}:\n Your yearly gross income {1}:\n Your total tax deduction {2}\n",
+                    arrayTax[x].SSN, arrayTax[x].GrossIncome.ToString("c"), arrayTax[x].Tax.ToString("c"));
+
+            }
+            Array.Sort(arrayTax);
+            for (x = 0; x < arrayTax.Length; x++)
+            {
+                WriteLine(arrayTax[x]);
+                WriteLine("Enter your Social Security Number.");
+                arrayTax[x].SSN = ReadLine();
+
+                WriteLine("Enter your yearly income salary.");
+                arrayTax[x].GrossIncome = double.Parse(ReadLine());
+                WriteLine();
+
+                WriteLine(" Your Social Security Number {0}:\n Your yearly gross income {1}:\n Your total tax deduction {2}\n",
+                    arrayTax[x].SSN, arrayTax[x].GrossIncome.ToString("c"), arrayTax[x].Tax.ToString("c"));
+            }
+            Read();
+        }
+        class Taxpayer2: IComparable
+        {
+
+            public string ssn;//{ get; set; }
+            public double grossIncome;//{get; set; }
+            public double tax;//{ get; set; }
+
+
+            public string SSN
+            {
+                get
+                {
+                    return this.ssn;
+
+                }
+                set
+                {
+                    this.ssn = value;
+
+                }
+            }
+
+            public double GrossIncome
+            {
+
+                get
+                {
+                    return this.grossIncome;
+
+                }
+                set
+                {
+                    this.grossIncome = value;
+                    if (grossIncome < 30000)
+                    {
+                        tax = grossIncome * .15;
+                    }
+                    else
+                    {
+                        tax = grossIncome * .28;
+                    }
+
+
+                }
+            }
+            public double Tax
+            {
+                get
+                {
+                    return tax;
+                }
+            }
+            int IComparable.CompareTo(object o)
+            {
+                int returnVal;
+                Taxpayer2 temp = (Taxpayer2)o;
+                if (this.Tax > temp.Tax)
+                {
+                    returnVal = 1;
+                }
+                else 
+                if (this.Tax < temp.Tax)
+                {
+                    returnVal = -1;
+                }
+                else
+                {
+                    returnVal = 0;
+                }
+                return returnVal;
+            }
+        }
+    }
+}
